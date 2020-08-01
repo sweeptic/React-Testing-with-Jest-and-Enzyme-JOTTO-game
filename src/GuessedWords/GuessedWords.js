24bd0f32946bfed938398ae8c802a6bf250b0e23
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-01 18:45:20 +0200

    5. Code for no words guessed

--- src/GuessedWords/GuessedWords.js
+++ src/GuessedWords/GuessedWords.js
@@ -9,11 +9,24 @@ import PropTypes from 'prop-types';
 * @param {object} props - React props.
 * @returns {JSX.Element} - Rendered component
 */
-const GuessedWords = (props) => (
-  <div data-test='component-guessedwords' className="GuessedWordsWrapper">
-    Test content
-  </div>
-);
+const GuessedWords = (props) => {
+  let contents;
+
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secret word!
+      </span>
+    )
+  }
+
+
+  return (
+    <div data-test='component-guessedwords' className="GuessedWordsWrapper">
+      {contents}
+    </div>
+  )
+};
 
 
 GuessedWords.propTypes = {
