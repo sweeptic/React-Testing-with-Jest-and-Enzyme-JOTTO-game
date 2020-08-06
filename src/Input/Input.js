2cf73794d91d6df6d2c08fc50bef89034e937e3f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-06 13:57:44 +0200

    8. Set Up Connected Component Tests

---
+++ src/Input/Input.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+class Input extends PureComponent {
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = state => {
+  return {};
+}
+
+
+export default connect(
+  mapStateToProps
+)(Input);
---
+++ src/Input/Input.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { configure, shallow, mount, render } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { findByTestAttr} from '../../test/testUtils'
+import Input from './Input';
+
+
+test('renders without error ', () => {
+  // const wrapper = setup();
+  // const inputComponent = findByTestAttr(wrapper, 'component-input');
+  // expect(inputComponent.length).toBe(1);
+})
+
+
+
+export default Input;
