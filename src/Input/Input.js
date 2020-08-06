00a1ac462a37684a10b723f096fcdec8124f8057
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-06 15:06:41 +0200

    14. Input Tests and Code for success=false

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,24 +1,42 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Input extends PureComponent {
+class Input extends Component {
 
 
   render() {
+    const contents = this.props.success ? null : (
+      <form className="form-inline" action="">
+        <input
+          data-test='input-box'
+          type="text"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+        />
+        <button
+          data-test='submit-button'
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    )
+
+
     return (
-      <div>
-        <button></button>
+      <div data-test='component-input' className="">
+        {contents}
       </div>
-    );
+    )
+
   }
 }
 
 
-const mapStateToProps = state => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 }
 
 
-export default connect(
-  mapStateToProps
-)(Input);
+export default connect(mapStateToProps)(Input);
