058de87e21494f0a66ef4cb39d21e10427cfae48
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-06 14:20:17 +0200

    10. Start setup Function for Input Component

--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -4,6 +4,19 @@ import { findByTestAttr } from '../../test/testUtils'
 import Input from './Input';
 
 
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState = {}) => {
+  const wrapper = shallow(<Input />);
+  console.log(wrapper.debug())
+}
+
+setup();
+
 describe('render', () => {
 
   describe('word has not been guessed', () => {
