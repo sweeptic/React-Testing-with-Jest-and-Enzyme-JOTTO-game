aca753db61506240a0bde91e52fa968ced3b47d6
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-09 15:13:03 +0200

    7. Quiz! Tests for  on Input Submit Click

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { guessWord } from '../actions';
 
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
 
   render() {
     const contents = this.props.success ? null : (
@@ -17,6 +17,7 @@ class Input extends Component {
         <button
           data-test='submit-button'
           className="btn btn-primary mb-2"
+          onClick={() => this.props.guessWord('train')}
           type="submit"
         >
           Submit
@@ -38,4 +39,4 @@ const mapStateToProps = ({ success }) => {
 }
 
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
