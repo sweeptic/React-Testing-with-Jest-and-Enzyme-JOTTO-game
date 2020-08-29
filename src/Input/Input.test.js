7575c646ea97e90e6df0f0cd1b45db8ffa528ea3
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-29 17:29:10 +0200

    12. Enzyme  method

--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -11,8 +11,8 @@ configure({ adapter: new EnzymeAdapter() });
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />);
-  console.log(wrapper.debug())
+  const wrapper = shallow(<Input store={store} />).dive().dive();
+  return wrapper;
 }
 
 setup()
