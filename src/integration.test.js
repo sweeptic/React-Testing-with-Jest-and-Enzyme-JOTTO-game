21cc3d0994b98347fa5457b66b4cd7a2b13c5e10
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-08 14:54:15 +0200

    7. First Thunk Test

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,11 +2,31 @@ import { storeFactory } from "./Utils/testUtils";
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
-
+   const secretWord = 'party';
+   const unsuccessfulGuess = 'train';
 
    describe('no guessed words', () => {
+      let store;
+      const initialState = { secretWord }
+      beforeEach(() => {
+         store = storeFactory(initialState)
+      })
       test('updates state correctly for unsuccessful guess ', () => {
-
+         //dispatch
+         store.dispatch(guessWord(unsuccessfulGuess));
+         //get the state
+         const newState = store.getState()
+         //define what we expect the state to be
+         const expectedState = {
+            ...initialState,
+            success: false,
+            guessedWords: [{
+               guessedWord: unsuccessfulGuess,
+               letterMatchCount: 3
+            }]
+         }
+         //expect
+         expect(newState).toEqual(expectedState)
       })
       test('updates state correctly for successful guess ', () => {
 
@@ -15,7 +35,7 @@ describe('guessWord action dispatcher', () => {
 
    describe('some guessed words', () => {
       test('updates state correctly for unsuccessful guess ', () => {
- 
+
       })
       test('updates state correctly for successful guess ', () => {
 
