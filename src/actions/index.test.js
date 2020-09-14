f3caed2d8c7c940ca6dc8b1a630d395a51ed1280
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-14 17:53:01 +0200

    5. Write  tests

---
+++ src/actions/index.test.js
@@ -0,0 +1,34 @@
+const moxios = require("moxios");
+import { getSecretWord } from '.';
+import { storeFactory } from '../Utils/testUtils';
+
+
+
+
+describe('get secret action creator', () => {
+   beforeEach(() => {
+      moxios.install();
+   })
+   afterEach(() => {
+      moxios.uninstall();
+   })
+   test('adds response word to state', () => {
+      const secretWord = 'party';
+      const store = storeFactory()
+
+
+      moxios.wait(() => {
+         const request = moxios.requests.mostRecent();
+         request.respondWith({
+            status: 200,
+            response: secretWord
+         })
+      })
+      return store.dispatch(getSecretWord())
+         .then(() => {
+            const newState = store.getState();
+            expect(newState.secretWord).toBe(secretWord)
+         });
+
+   })
+});
