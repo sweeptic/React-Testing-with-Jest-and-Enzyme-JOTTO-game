d672e6eb74c101cd985a2835db5d914df2a9ef04
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-22 16:56:24 +0100

    write unit tests for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+const Congrats = props => {
+  const content = props.success ? (
+    <div data-test='component-message'>Congratulations!</div>
+  ) : null;
+
+  return <div data-test='component-congrats'>{content}</div>;
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+};
+
+export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,4 +1,40 @@
-// test('renders without error', () => {});
-// test('renders no text when `success` prop is false', () => {});
-// test('renders non-empty congrats message when `success` prop is true', () => {});
-// test('does not throw warning with expected props', () => {});
+import React from 'react';
+import Congrats from './Congrats';
+import { shallow } from 'enzyme';
+import { checkProps, findByTestAttr } from './test/testUtils';
+
+const defaultProps = { success: false };
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
+};
+
+test('renders  without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
+});
+
+test('renders no text when `success` prop is false ', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe('');
+});
+
+test('renders non-empty congrats message when `success` prop is true ', () => {
+  const wrapper = setup({ success: true });
+  const component = findByTestAttr(wrapper, 'component-message');
+  expect(component.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props ', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
+});
