8123b30987fe405069068ab4979b11f3c8117cc2
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-22 15:03:07 +0100

    fix some test cases.

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,5 +1,3 @@
-test('does not throw warning with expected props', () => {});
-
 describe('if there are no words guessed', () => {
   test('renders without error', () => {});
   test('renders instructions to guess a word', () => {});
@@ -10,3 +8,5 @@ describe('if there are words guessed', () => {
   test('renders "guessed words" section', () => {});
   test('correct number of guessed words', () => {});
 });
+
+test('does not throw warning with expected props', () => {});
--- src/integration.test.js
+++ src/integration.test.js
@@ -1,7 +1,8 @@
 describe('guessWord action dispatcher', () => {
-  describe('no guessed words', () => {});
-  test('updates state correctly for unsuccessful guess', () => {});
-  test('updates state correctly for successful guess', () => {});
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {});
+    test('updates state correctly for successful guess', () => {});
+  });
   describe('some guessed words', () => {
     test('updates state correctly for unsuccessful guess', () => {});
     test('updates state correctly for successful guess', () => {});
