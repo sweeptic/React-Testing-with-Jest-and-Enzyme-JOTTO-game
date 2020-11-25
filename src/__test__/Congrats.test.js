b22ceae569e29e4f9b30d5d6ff735b254196c203
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-25 17:10:05 +0100

    revert to basic setup - practice react test!

--- src/__test__/Congrats.test.js
+++ src/__test__/Congrats.test.js
@@ -1,45 +1,4 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { checkProps, findByTestAttr } from '../test/testUtils';
-import Congrats from '../Congrats';
-
-// --render component
-// --render - props success true - show congratulation message
-// --render - props success false - donst show anything
-// --proptypes - dont show wrning when expected props
-
-const defaultProps = { success: false };
-
-/**
- * Factory function to create a ShallowWrapper for the  component.
- * @function setup
- * @param {object} props - Component props specific to this setup.
- * @returns {ShallowWrapper}
- */
-const setup = (props = {}) => {
-  const setupProps = { ...defaultProps, ...props };
-  return shallow(<Congrats {...setupProps} />);
-};
-
-test('render component ', () => {
-  const wrapper = setup();
-  const component = findByTestAttr('wrapper', 'component-congrats');
-  expect(component.length).toBe(1);
-});
-
-test('renders no text when `success` prop is false ', () => {
-  const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, 'component-congrats');
-  expect(component.text()).toBe('');
-});
-
-test('renders non-empty congrats message when `success` prop is true ', () => {
-  const wrapper = setup({ success: true });
-  const component = findByTestAttr(wrapper, 'congrats-message');
-  expect(component.text().length.not.toBe(0));
-});
-
-test('proptypes - dont show warning when expected props ', () => {
-  const expectedProps = { success: false };
-  checkProps(Congrats, expectedProps);
-});
+test('render component ', () => {});
+test('renders no text when `success` prop is false ', () => {});
+test('renders non-empty congrats message when `success` prop is true ', () => {});
+test('proptypes - dont show warning when expected props ', () => {});
