ec4fef792043cfa9f7cd87b0a5d39e303c212958
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-28 18:23:47 +0100

    input tests green stage for success=false

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Input = props => {
-  return <div></div>;
+const Input = ({ success }) => {
+  let content = !success ? (
+    <form className='form-inline'>
+      <input
+        data-test='input-box'
+        className='mb-2 mx-sm-3'
+        type='text'
+        placeholder='enter guess'
+      />
+      <button
+        data-test='submit-button'
+        type='submit'
+        className='btn btn-primary mb-2'
+      >
+        Submit
+      </button>
+    </form>
+  ) : null;
+
+  return <div data-test='component-input'>{content}</div>;
 };
 
-const mapStateToProps = state => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 // const mapDispatchToProps = dispatch => ({
