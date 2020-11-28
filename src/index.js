022bc0f8303c8f6afafb325d43e2be44cf534d49
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-28 17:17:00 +0100

    set up redux barebone

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
+);
 registerServiceWorker();
