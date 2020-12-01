20d5e47f0a24f30cfde82520c123c82238c481ab
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-01 14:13:21 +0100

    input component test cases - red stage

--- src/Input.js
+++ src/Input.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
 
+const Input = state => {
+  return (
+    <div>
+      <button></button>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { success: state.success };
+};
+
+export default connect(mapStateToProps)(Input);
