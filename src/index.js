36468a259c147de9c6330615faacd855d04d1017
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-01 13:07:32 +0100

    code for success reducer shell

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './configureStore';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
   document.getElementById('root')
 );
