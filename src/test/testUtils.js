7f592840d0e8e8d7c33b58b087da86afe6f272fe
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-01 15:26:05 +0100

    set up middlewares in application and storefactory jsdoc

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,5 +1,11 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { middlewares } from '../configureStore';
+import rootReducer from '../reducers/index';
+
+import rootReducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { middlewares } from '../configureStore';
 import rootReducer from '../reducers/index';
 
 /**
@@ -10,7 +16,10 @@ import rootReducer from '../reducers/index';
  * @returns {Store} - Redux store.
  */
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
