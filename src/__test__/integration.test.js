5694a7fcfc15e18974bf8e9f1244d52fb80fc111
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-09 18:15:15 +0100

    test cases for no guessed words, and unsuccessfull guess

--- src/__test__/integration.test.js
+++ src/__test__/integration.test.js
@@ -1,8 +1,35 @@
+import { guessWord } from '../actions';
+import { storeFactory } from '../test/testUtils';
+
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfullguess = 'train';
+
+  let store;
+
   describe('no guessed words', () => {
-    test('updates state correctly for unsuccessful guess', () => {});
+    const initialState = { guessedWords: [], secretWord: secretWord };
+    store = storeFactory(initialState);
+
+    test('updates state correctly for unsuccessful guess', () => {
+      store.dispatch(guessWord(unsuccessfullguess));
+
+      const newState = store.getState();
+
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          { guessedWord: unsuccessfullguess, letterMatchCount: 3 },
+        ],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
     test('updates state correctly for successful guess', () => {});
-    describe('some guessed words', () => {});
+  });
+
+  describe('some guessed words', () => {
     test('updates state correctly for unsuccessful guess', () => {});
     test('updates state correctly for successful guess', () => {});
   });
