81cf3a13beffea4b735f85480ff5d98761a28c73
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-12 15:30:05 +0100

    revert to test axios - async action integration test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,8 @@
-import Axios from 'axios';
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
-  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 export const guessWord = guessedWord => {
@@ -19,19 +17,5 @@ export const guessWord = guessedWord => {
         letterMatchCount: letterMatchCount,
       },
     });
-  };
-};
-
-export const getSecretWord = () => {
-  return dispatch => {
-    const res = Axios.get('http://localhost:3030').then(response => {
-      dispatch({
-        type: actionTypes.SET_SECRET_WORD,
-        payload: response.data,
-      });
-      console.log('run then');
-    });
-
-    return res;
   };
 };
---
+++ src/actions/index.test_.js
@@ -0,0 +1,8 @@
+// import { correctGuess, actionTypes } from '.';
+
+// describe('correct guess', () => {
+//   test('return an action with type "CORRECT GUESS"', () => {
+//     const action = correctGuess();
+//     expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+//   });
+// });
