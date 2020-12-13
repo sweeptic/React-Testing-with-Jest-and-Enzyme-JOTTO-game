e8ab9a712f019d1ea2d21efe97580013bb9178dc
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-13 11:27:37 +0100

    revert to axios async test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,38 +1,3 @@
-import moxios from 'moxios';
-import { getSecretWord } from '.';
-import { storeFactory } from '../test/testUtils';
-
-describe('get secret word and test store', () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-
-  afterEach(() => {
-    moxios.uninstall();
-  });
-
-  test('set secret word to store', () => {
-    const secretWord = 'party';
-    const store = storeFactory();
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 200,
-        response: secretWord,
-      });
-    });
-
-    return store.dispatch(getSecretWord()).then(() => {
-      // console.log('response received from action test');
-      const newState = store.getState().secretWord;
-      const expectedState = secretWord;
-      expect(newState).toEqual(expectedState);
-      // console.log(store.getState());
-    });
-  });
-});
-
 // import { correctGuess, actionTypes } from '.';
 
 // describe('correct guess', () => {
