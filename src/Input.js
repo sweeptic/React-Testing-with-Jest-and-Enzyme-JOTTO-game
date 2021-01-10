357c1f97e899b724b9b4f70d42eab2a8730da745
unknown <sweeptic@gmail.com>
2021-01-10 10:37:08 +0100

    try new cloned git repo

--- src/Input.js
+++ src/Input.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-const Input = ({ secretWord }) => {
-  const [currentGuess, setCurrentGuess] = React.useState('');
-
-  return (
-    <div data-test='component-app'>
-      <form action=''>
-        <input
-          type='text'
-          data-test='input-box'
-          value={currentGuess}
-          onChange={event => setCurrentGuess(event.target.value)}
-        />
-        <button
-          type='submit'
-          data-test='submit-button'
-          onClick={evt => {
-            evt.preventDefault();
-            setCurrentGuess('');
-          }}
-        ></button>
-      </form>
-    </div>
-  );
+const Input = () => {
+  return <div></div>;
 };
 
 //simple proptypes
-Input.propTypes = {
-  secretWord: PropTypes.string.isRequired,
-};
+// Input.propTypes = {
+//   secretWord: PropTypes.string.isRequired,
+// };
+
+// //array of object proptypes
+// Input.propTypes = {
+//   Input: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       secretWord: PropTypes.string.isRequired,
+//       prop: PropTypes.string.isRequired,
+//     })
+//   ).isRequired,
+// };
 
 export default Input;
