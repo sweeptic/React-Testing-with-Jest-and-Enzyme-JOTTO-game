8a1fc7654d2d917a706d5f24a71947d29d367284
unknown <sweeptic@gmail.com>
2021-01-10 13:21:57 +0100

    testing input component

--- src/Input.js
+++ src/Input.js
@@ -1,23 +1,36 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const Input = () => {
-  return <div></div>;
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test='component-input'>
+      <form>
+        <input
+          type='text'
+          data-test='input-box'
+          value={currentGuess}
+          onChange={evt => {
+            setCurrentGuess(evt.target.value);
+          }}
+        />
+        <button
+          type='submit'
+          data-test='submit-button'
+          onClick={evt => {
+            evt.preventDefault();
+            setCurrentGuess('');
+          }}
+        ></button>
+      </form>
+    </div>
+  );
 };
 
 //simple proptypes
-// Input.propTypes = {
-//   secretWord: PropTypes.string.isRequired,
-// };
-
-// //array of object proptypes
-// Input.propTypes = {
-//   Input: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       secretWord: PropTypes.string.isRequired,
-//       prop: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-// };
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,9 +1,45 @@
-test('Input renders without error ', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import Input from './Input';
+import { checkProps, findByTestAttr } from '../test/testUtils';
 
-test('propTypes - does not throw warning with expected props ', () => {});
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
+};
+
+test('Input renders without error ', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-input');
+  expect(component.length).toBe(1);
+});
+
+test('propTypes - does not throw warning with expected props ', () => {
+  const expectedProps = { secretWord: 'party' };
+  checkProps(Input, expectedProps);
+});
 
 describe('state controlled input field', () => {
-  test('state updates with value of input box upon change', () => {});
+  let wrapper;
+  let mockSetCurrentGuess = jest.fn();
 
-  test('field is cleared upon submit button click', () => {});
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test('state updates with value of input box upon change', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = { target: { value: 'train' } };
+
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('field is cleared upon submit button click', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+  });
 });
