394731779bb6c92a496db25ebfed79916f5ac701
unknown <sweeptic@gmail.com>
2021-01-10 13:55:38 +0100

    hookActions test

--- src/actions/hookActions.js
+++ src/actions/hookActions.js
@@ -1,1 +1,8 @@
+import Axios from 'axios';
 
+export const getSecretWord = async setSecretWord => {
+  const response = await Axios.get('http://localhost:3030');
+  setSecretWord(response.data);
+};
+
+export default { getSecretWord };
--- src/actions/hookActions.test.js
+++ src/actions/hookActions.test.js
@@ -1,1 +1,27 @@
-test('calls the getSecretWord callback on axios response', () => {});
+import moxios from 'moxios';
+import { getSecretWord } from './hookActions';
+
+beforeEach(() => {
+  moxios.install();
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+test('calls the getSecretWord callback on axios response', async () => {
+  const secretWord = 'party';
+
+  moxios.wait(() => {
+    const request = moxios.requests.mostRecent();
+    request.respondWith({
+      status: 200,
+      response: secretWord,
+    });
+  });
+
+  const mockSetSecretWord = jest.fn();
+
+  await getSecretWord(mockSetSecretWord);
+  expect(mockSetSecretWord).toHaveBeenCalled();
+});
