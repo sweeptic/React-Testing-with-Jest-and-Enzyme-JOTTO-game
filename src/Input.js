b8f030c0015089cedfc20ae0a86daf708d91c41c
unknown <sweeptic@gmail.com>
2021-01-12 09:47:09 +0100

    input render test - red stage

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = () => {
+  return <div></div>;
+};
+
+//simple proptypes
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
+
+export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,4 +1,17 @@
-test('Input renders without error ', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import Input from './Input';
+import { findByTestAttr } from '../test/testUtils';
+
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
+};
+
+test('Input renders without error ', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
+});
 
 test('propTypes - does not throw warning with expected props ', () => {});
 
