08db924039efb6e8307f6790f2223ec403c5dc9c
unknown <sweeptic@gmail.com>
2021-01-13 15:19:49 +0100

    begin test language picker component and strings function

--- src/LanguagePicker.js
+++ src/LanguagePicker.js
@@ -1,28 +1,4 @@
-import React from 'react';
-import propTypes from 'prop-types';
-
-const LanguagePicker = ({ setLanguage }) => {
-  const languages = [
-    { code: 'en', symbol: '🇺🇸' },
-    { code: 'emoji', symbol: '😊' },
-  ];
-
-  const languageIcons = languages.map(lang => (
-    <span
-      data-test='language-icon'
-      key={lang.code}
-      onClick={() => setLanguage(lang.code)}
-    >
-      {lang.symbol}
-    </span>
-  ));
-
-  return <div data-test='component-languagePicker'>{languageIcons}</div>;
-};
-
-//simple propTypes
-LanguagePicker.propTypes = {
-  setLanguage: propTypes.func.isRequired,
-};
-
-export default LanguagePicker;
+const languages = [
+  { code: 'en', symbol: '🇺🇸' },
+  { code: 'emoji', symbol: '😊' },
+];
