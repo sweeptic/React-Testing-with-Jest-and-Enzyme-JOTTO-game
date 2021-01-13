8f33426e684c1b5b2fe9c79593c9224e8962fcf5
unknown <sweeptic@gmail.com>
2021-01-13 14:01:31 +0100

    test language picker component and strings function

--- src/LanguagePicker.test.js
+++ src/LanguagePicker.test.js
@@ -1,7 +1,35 @@
-test('renders without error', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import LanguagePicker from './LanguagePicker';
+import { checkProps, findByTestAttr } from '../test/testUtils';
 
-test('does not throw warning with expected props', () => {});
+const mockSetLanguage = jest.fn();
 
-test('renders non-zero language icons', () => {});
+const setup = () => {
+  return shallow(<LanguagePicker setLanguage={mockSetLanguage} />);
+};
 
-test('calls setLanguage prop upon click', () => {});
+test('LanguagePicker renders without error ', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-languagePicker');
+  expect(component.exists()).toBe(true);
+});
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { setLanguage: jest.fn() };
+  checkProps(LanguagePicker, expectedProps);
+});
+
+test('renders non-zero language icons', () => {
+  const wrapper = setup();
+  const languageIcons = findByTestAttr(wrapper, 'language-icon');
+  expect(languageIcons.length).toBeGreaterThan(0);
+});
+
+test('calls setLanguage prop upon click', () => {
+  const wrapper = setup();
+  const languageIcons = findByTestAttr(wrapper, 'language-icon');
+  const firstIcon = languageIcons.first();
+  firstIcon.simulate('click');
+  expect(mockSetLanguage).toHaveBeenCalled();
+});
