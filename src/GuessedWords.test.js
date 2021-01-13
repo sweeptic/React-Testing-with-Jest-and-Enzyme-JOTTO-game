23a4e63a1e82103735c56bc9fa8f29a04445ff6e
unknown <sweeptic@gmail.com>
2021-01-13 11:21:37 +0100

    tests and code language context for guessedWords component

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -8,22 +8,30 @@ const defaultProps = {
 };
 
 /**
-* Factory function to create a ShallowWrapper for the GuessedWords component.
-* @function setup
-* @param {object} props - Component props specific to this setup.
-* @returns {ShallowWrapper}
-*/
-const setup = (props={}) => {
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<GuessedWords {...setupProps } />)
+  return shallow(<GuessedWords {...setupProps} />);
 };
+
+// const setup = (guessedWords = []) => {
+//   const mockUseGuessedWords = jest
+//     .fn()
+//     .mockReturnValue([guessedWords, jest.fn()]);
+//   guessedWordsContext.useGuessedWords = mockUseGuessedWords;
+//   return shallow(<GuessedWords />);
+// };
 
 test('does not throw warning with expected props', () => {
   checkProps(GuessedWords, defaultProps);
 });
 
 describe('if there are no words guessed', () => {
-  let wrapper
+  let wrapper;
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
@@ -46,7 +54,7 @@ describe('if there are words guessed', () => {
   beforeEach(() => {
     wrapper = setup({ guessedWords });
   });
-  test ('renders without error', () => {
+  test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-guessed-words');
     expect(component.length).toBe(1);
   });
@@ -57,5 +65,20 @@ describe('if there are words guessed', () => {
   test('correct number of guessed words', () => {
     const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
     expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
+});
+
+describe('languagePicker', () => {
+  test('correctly renders guess instructions string in English by default', () => {
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(guessInstructions.text()).toBe('Try to guess the secret word!');
+  });
+  test('correctly renders guess instructions string in emoji', () => {
+    const mockUseContext = jest.fn().mockReturnValue('emoji');
+    React.useContext = mockUseContext;
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(guessInstructions.text()).toBe('🤔🤫🔤');
   });
 });
