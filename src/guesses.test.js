6e541150ef004f6006b800cff660c65da301b629
unknown <sweeptic@gmail.com>
2021-01-21 14:18:34 +0100

    11. Set up Tests for Input Setting Success Context

--- src/guesses.test.js
+++ src/guesses.test.js
@@ -1,19 +1,29 @@
-// prepopulate guessedWords context by simulating word guessedWords
+import React from 'react';
+import { mount } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+
+import successContext from './contexts/successContext';
+import Input from './Input';
+
+function setup(secretWord = 'party') {
+  const wrapper = mount(
+    <successContext.SuccessProvider>
+      <Input secretWord={secretWord} />
+    </successContext.SuccessProvider>
+  );
+
+  const inputBox = findByTestAttr(wrapper, 'input-box');
+  const submitButton = findByTestAttr(wrapper, 'submit-button');
+
+  return [wrapper, inputBox, submitButton];
+}
 
 describe('test word guesses', () => {
-  describe('non-empty guessedWords', () => {
-    describe('correct guess', () => {
-      test('Input component contains no children', () => {});
-      test('GuessedWords table row count reflects updated guess', () => {});
-    });
+  let wrapper;
+  let inputBox;
+  let submitButton;
 
-    describe('incorrect guess', () => {
-      test('Input box remains', () => {});
-      test('GuessedWords table row count reflects updated guess', () => {});
-    });
-  });
-
-  describe('empty guessWords', () => {
-    test('guessedWords shows correct guesses after incorrect guess', () => {});
+  beforeEach(() => {
+    [wrapper, inputBox, submitButton] = setup('party');
   });
 });
